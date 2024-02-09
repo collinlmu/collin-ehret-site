bc25273f9c3cfbb1664d216242bbcc1f0fa6d1e8
Collin Ehret <collin.ehret@gmail.com>
2024-02-09 10:51:11 -0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import ListGroup from "./components/ListGroup";
+
+function App() {
+    let items = ["New York", "SF", "Tokyo", "Paris", "London"];
+
+    const handleSelectItem = (item: string) => {
+        console.log(item);
+    };
+    return (
+        <div>
+            <ListGroup
+                items={items}
+                heading="Cities"
+                onSelectItem={handleSelectItem}
+            />
+        </div>
+    );
+}
+
+export default App;
